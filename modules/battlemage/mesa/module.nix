cd14e955228065a779a51d0a7e0e34ad450ee8ba
Soveu <marx.tomasz@gmail.com>
2026-07-10 20:37:53 +0200

    update mesa

--- modules/battlemage/mesa/module.nix
+++ modules/battlemage/mesa/module.nix
@@ -19,6 +19,7 @@
               src = inputs.git-mesa;
               version = "26.2.0-devel";
               buildInputs = prev.mesa.buildInputs ++ [ prev.libdisplay-info ];
+              patches = prev.mesa.patches ++ [ ./format-error.patch ];
             }).override
               {
                 stdenv = final.withCFlags [ "-march=znver5" "-mtune=znver5" ] final.gcc15Stdenv;
